a7ab31699ce7a69ed56c4f3c60dd8e4481362c07
Macias10 <mlmacias@alumno.montepiedra.edu.ec>
2025-10-27 03:20:19 -0500

    Add files via upload

---
+++ ejercicio_495.cpp
@@ -0,0 +1,9 @@
+// Título: Analizador de rendimiento
+
+
+// Programa de ejemplo en C++
+// Descripción: ejercicio número 495
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){ string s; if(!(cin>>s)) return 0; string t=s; reverse(t.begin(), t.end()); cout<<(s==t?"PALINDROMO":"NO")<<"\n"; }
